28dccfc97b3641342d004ec408ea9f91d6ed6100
texasjkee <gt.gluck@gmail.com>
2023-02-24 13:11:51 +0200

    add: data verification condition

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
+const data = new Date();
+const day = data.getDate();
 const div = document.createElement('div');
+
 div.classList.add('truck_extention','hide');
 div.textContent = "Reloading";
 document.body.append(div);
 
-let reloadStatus = false;
+const JUDGMENT_DAY = 28;
 
-const reload = () => {
-  if(reloadStatus) { 
-    document.querySelector('#refresh-results').click();
-  } else {
-    console.log('click')
+if(day === JUDGMENT_DAY) {
+  div.innerHTML = `<a href="https://www.youtube.com/watch?v=9X_Ne3S5FAU&ab_channel=MaximPopov">CLICK ME</a>`   
+  div.classList.toggle('hide')
+} else {
+  let reloadStatus = false;
+
+  const reload = () => {
+    if(reloadStatus) { 
+      document.querySelector('#refresh-results').click();
+      console.log('reload')
+    } else {
+      console.log('click')
+    }
   }
+
+  setInterval(reload, 500);
+
+  document.addEventListener('keyup', e => {
+    if (e.code === 'ControlLeft') {
+      div.classList.toggle('hide')
+      reloadStatus = !reloadStatus
+    }
+  })
 }
-
-const start = setInterval(reload, 1000);
-
-document.addEventListener('keyup', e => {
-  if (e.code === 'ControlLeft') {
-    div.classList.toggle('hide')
-    reloadStatus = !reloadStatus
-  }
-})
