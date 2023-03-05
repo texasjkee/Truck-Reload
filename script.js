2e0ef1f71221b87c3f9f55f1f1b6b599712ce810
texasjkee <gt.gluck@gmail.com>
2023-03-05 15:39:44 +0200

    fix: judgment_day

--- script.js
+++ script.js
@@ -6,12 +6,6 @@ div.classList.add('truck_extention','hide');
 div.textContent = "Reloading";
 document.body.append(div);
 
-const JUDGMENT_DAY = 29;
-
-if(day === JUDGMENT_DAY) {
-  div.innerHTML = `<a href="https://www.youtube.com/watch?v=9X_Ne3S5FAU&ab_channel=MaximPopov">CLICK ME</a>`   
-  div.classList.toggle('hide')
-} else {
   let reloadStatus = false;
 
   const reload = () => {
@@ -31,4 +25,3 @@ if(day === JUDGMENT_DAY) {
       reloadStatus = !reloadStatus
     }
   })
-}
